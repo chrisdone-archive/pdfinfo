09bd1f34cfcebe5c665b25f4cd533d1c2ecea051
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2011-02-24 09:39:20 -0500

    Removed trailing withespace

--- src/Text/PDF/Info.hs
+++ src/Text/PDF/Info.hs
@@ -103,9 +103,9 @@ parse out = runParse $
 
 -- | Parse a page size. This is loosely defined.
 parseSize :: String -> ParsePDFInfo PDFSize
-parseSize s = 
+parseSize s =
   case words s of
-    ((readRight -> Right x):"x":(readRight -> Right y):_) -> 
+    ((readRight -> Right x):"x":(readRight -> Right y):_) ->
         return $ PDFSize x y
     _ -> throwError $ ParseError $ "Unable to read size: " ++ show s
 
